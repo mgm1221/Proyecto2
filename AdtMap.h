fc1fe2f13063b6f494920e5374eac9b4e72377b5
martin <martingarcesm@gmail.com>
2022-07-10 17:45:18 -0400

    mapavl.cpp

--- AdtMap.h
+++ AdtMap.h
@@ -1,19 +1,19 @@
-#ifndef ADTMAP_H
-#define ADTMAP_H
+#ifndef  ADT_MAP_H
+#define ADT_MAP_H
+
 #include <string>
 
 using namespace std;
 
-class AdtMap{
-
-    //key = string, value = int
-    virtual void insert(const string &,int) = 0;// insertar par
-    virtual void erase(const string &) = 0; //borrar un par clave valor
-    virtual int at(const string &) = 0; // encontrar el valor asociado a una clave
-    virtual int size() = 0; //devuelve el tamano
-    virtual bool empty() = 0;// verifica si esta vacio
+class ADTMap{
+public:
+  virtual void insert(const string&, int)=0;
+  virtual void erase(const string&)=0;
+  virtual int at(const string&)=0;
+  virtual int size()=0;
+  virtual bool empty()=0;
+  
 
 };
-
 
 #endif
--- MapAvl.h
+++ MapAvl.h
@@ -1,27 +1,10 @@
-#ifndef MAPAVL_H
-#define MAPAVL_H
-#include <string>
+#ifndef MAP_AVL_H
+#define MAP_AVL_H
 
-class MapAVL{
-    struct Nodo
-    {
-
-        pair<string, int> par;
-        Node* right;
-        Node* left;
-
-    };
-    private:
-
-        Nodo raiz;
-
-    public:
-        void insert(const string &, int);
-        void erase(const string &);
-        int at(const string &);
-        int size();
-        bool empty();
-
-};
+struct node {
+    pair<<string,int>>;
+    struct avl_node* left;
+    struct avl_node* right;
+}
 
 #endif
