61696f91cc59e13f76b0f7b77170e036b8975ce0
Martin <martingarcesm@gmail.com>
2022-07-17 11:13:50 -0400

    f

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(){
 
     MapAVL* mapavl = new MapAVL();
     string s;
-    for (int i = 0; i < 10000; i++)
+    for (int i = 0; i < 1000; i++)
     {
         cin>>s;
         mapavl->insert(s,i);
